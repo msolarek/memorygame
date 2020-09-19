9209420c8abf7a3e2291f862bf7c17cabe620030
KamilO <wheels4@o2.pl>
2020-09-19 12:42:08 +0200

    initial commit

---
+++ src/test/java/pl/project/memorygame/MemorygameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.memorygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorygameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
